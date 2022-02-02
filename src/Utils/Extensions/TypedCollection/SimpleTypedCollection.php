83253cbc251d8eaba8548cbea57423d8d79905b6
Yurii Orlyk <orlyk.rollun@gmail.com>
2022-02-02 02:39:21 +0200

    added SimpleTypedCollection class

---
+++ src/Utils/Extensions/TypedCollection/SimpleTypedCollection.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Clean\Common\Utils\Extensions\TypedCollection;
+
+class SimpleTypedCollection extends TypedCollection
+{
+    protected $type;
+
+    public function __construct(string $type, object|array $array = [], int $flags = 0)
+    {
+        $this->type = $type;
+        parent::__construct($array, $flags);
+    }
+
+    protected function getType()
+    {
+        return $this->type;
+    }
+}
