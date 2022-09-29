5a8585d7237190d1e4c2fc577fa7657807b12dca
Yurii Orlyk <orlyk.rollun@gmail.com>
2022-09-29 22:19:45 +0000

    added some methods to traits

--- src/Domain/Traits/ProblemTrait.php
+++ src/Domain/Traits/ProblemTrait.php
@@ -3,30 +3,54 @@
 namespace Clean\Common\Domain\Traits;
 
 use Clean\Common\Utils\Extensions\ArrayObject\ArrayObject;
-use Clean\Common\Utils\Extensions\ArrayObject\ArrayObjectItem;
+use Clean\Common\Utils\Extensions\ArrayObject\ArrayObjectItemInterface;
 
 trait ProblemTrait
 {
     /**
-     * @var ArrayObject
+     * @var ArrayObject|null
      */
     protected $problems;
 
-    public function addProblem($problem)
+    /**
+     * @param ArrayObjectItemInterface $problem
+     * @return void
+     */
+    public function addProblem(ArrayObjectItemInterface $problem)
     {
         if ($this->problems === null) {
             $this->problems = new ArrayObject(true);
         }
 
-        $this->problems->addItem(new ArrayObjectItem($problem));
+        $this->problems->addItem($problem);
     }
 
-    public function hasProblem($problem)
+    /**
+     * @param $problem
+     * @return bool
+     */
+    public function hasProblem($problem): bool
     {
         if (isset($this->problems)) {
             return $this->problems->hasItem($problem);
         }
 
         return false;
+    }
+
+    /**
+     * @return ArrayObject|null
+     */
+    public function getProblems(): ?ArrayObject
+    {
+        return $this->problems;
+    }
+
+    /**
+     * @param ArrayObject|null $problems
+     */
+    public function setTags(?ArrayObject $problems): void
+    {
+        $this->problems = $problems;
     }
 }
--- src/Domain/Traits/TagTrait.php
+++ src/Domain/Traits/TagTrait.php
@@ -3,30 +3,54 @@
 namespace Clean\Common\Domain\Traits;
 
 use Clean\Common\Utils\Extensions\ArrayObject\ArrayObject;
-use Clean\Common\Utils\Extensions\ArrayObject\ArrayObjectItem;
+use Clean\Common\Utils\Extensions\ArrayObject\ArrayObjectItemInterface;
 
 trait TagTrait
 {
     /**
-     * @var ArrayObject
+     * @var ArrayObject|null
      */
     protected $tags;
 
-    public function addTag($tag)
+    /**
+     * @param ArrayObjectItemInterface $tag
+     * @return void
+     */
+    public function addTag(ArrayObjectItemInterface $tag): void
     {
         if ($this->tags === null) {
             $this->tags = new ArrayObject(true);
         }
 
-        $this->tags->addItem(new ArrayObjectItem($tag));
+        $this->tags->addItem($tag);
     }
 
-    public function hasTag($tag)
+    /**
+     * @param $tag
+     * @return bool
+     */
+    public function hasTag($tag): bool
     {
         if (isset($this->tags)) {
             return $this->tags->hasItem($tag);
         }
 
         return false;
+    }
+
+    /**
+     * @return ArrayObject|null
+     */
+    public function getTags(): ?ArrayObject
+    {
+        return $this->tags;
+    }
+
+    /**
+     * @param ArrayObject|null $tags
+     */
+    public function setTags(?ArrayObject $tags): void
+    {
+        $this->tags = $tags;
     }
 }
